4ab4b148edbad2e6278b006274170ab8820a0cca
Daniil Voroshin <kotanoff@raikage.ru>
2024-06-23 17:36:20 +0300

    0️⃣ upload script

---
+++ README-EN.md
@@ -0,0 +1,52 @@
+> [!NOTE]
+> - Контакты: [Telegram](https://t.me/mudachyo) 
+> - Канал: [Telegram Channel](https://t.me/shopalenka) 
+> - 🇪🇳 README in english available [here](README-EN.md)
+---
+## Как запустить  
+- Install extension in your browser [Violentmonkey](https://chromewebstore.google.com/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag?hl=be)
+- In order for BUMP to open in a browser, install [этот скрипт](https://github.com/mudachyo/BUMP/raw/main/bump-web.user.js)
+- Open [Bot BUMP](https://web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3DMMproBump_bot%26start%3Dref_2475526) and run the game
+
+- ![Результат](result.gif)
+
+---
+> [!TIP]
+> Other repositories:
+> 
+> - [Hamster Kombat + Autoclicker](https://github.com/mudachyo/Hamster-Kombat)
+> 
+> - [TapSwap + Autoclicker](https://github.com/mudachyo/TapSwap)
+> 
+> - [Blum + Autoclicker](https://github.com/mudachyo/Blum)
+>
+> - [PixelTap + Autoclicker](https://github.com/mudachyo/PixelTap)
+> 
+> - [MemeFi + Autoclicker](https://github.com/mudachyo/MemeFi-Coin)
+>
+> - [Yescoin + Autoclicker](https://github.com/mudachyo/Yescoin)
+>
+> - [Gemz + Autoclicker](https://github.com/mudachyo/Gemz)
+>
+> - [Crossfi + Autoclicker](https://github.com/mudachyo/Crossfi)
+>
+> - [BUMP](https://github.com/mudachyo/BUMP)
+>
+> - [W-Coin](https://github.com/mudachyo/W-Coin)
+---
+> [!IMPORTANT] 
+> Пожертвование
+> 
+> Мы принимаем следующие криптовалюты:
+> 
+> - **TON**: `UQCGUzPN5GnFqWJiYsFtqqLGO75-cBXlOL8f_qbd7yKY2Tzh`
+> 
+> - **USDT**(TRC20): `TFr8CiAPqEnSyoXHtVefWumodcXgjoB8rS`
+> 
+> - **USDT**(TON): `UQCGUzPN5GnFqWJiYsFtqqLGO75-cBXlOL8f_qbd7yKY2Tzh`
+> 
+> - **NOTCOIN**(TON): `UQCGUzPN5GnFqWJiYsFtqqLGO75-cBXlOL8f_qbd7yKY2Tzh`
+> 
+> - **BTC**: `1Mba8xKKVLdcFJdV7jD8Ba3fFn7DWbp4bt`
+> 
+> Пожертвования будут использованы для поддержания/сохранения проекта.
---
+++ bump-web.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         BUMP web
+// @version      1.0
+// @author       mudachyo
+// @match        *://bot.crossfi.org/*
+// @run-at       document-start
+// @icon         https://sun9-16.userapi.com/impg/g93WJxPJtCopVIAqWbzvaGWbkn-LaY-pEyCh1Q/Jvut2jMz7Rc.jpg?size=640x640&quality=95&sign=2f4460fee947b101894f4c274a599bff&c_uniq_tag=hQ43e9wzpBH6S4E0mWytfXtp5gde33mgCvmnGLVj-rI&type=album
+// @grant        none
+// @downloadURL  https://github.com/mudachyo/BUMP/raw/main/bump-web.user.js
+// @updateURL    https://github.com/mudachyo/BUMP/raw/main/bump-web.user.js
+// @homepage     https://github.com/mudachyo/BUMP
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var newUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
+
+    Object.defineProperty(navigator, 'userAgent', {
+        get: function() { return newUserAgent; }
+    });
+
+    Object.defineProperty(navigator, 'platform', {
+        get: function() { return 'iPhone'; }
+    });
+
+    Object.defineProperty(navigator, 'vendor', {
+        get: function() { return 'Apple Computer, Inc.'; }
+    });
+    function emitTouchEvent(event, touchType) {
+        var touchObj = new Touch({
+            identifier: Date.now(),
+            target: event.target,
+            clientX: event.clientX,
+            clientY: event.clientY,
+            screenX: event.screenX,
+            screenY: event.screenY,
+            pageX: event.pageX,
+            pageY: event.pageY,
+            radiusX: 2.5,
+            radiusY: 2.5,
+            rotationAngle: 10,
+            force: 0.5,
+        });
+
+        var touchEvent = new TouchEvent(touchType, {
+            cancelable: true,
+            bubbles: true,
+            shiftKey: true,
+            touches: [touchObj],
+            targetTouches: [touchObj],
+            changedTouches: [touchObj],
+        });
+
+        event.target.dispatchEvent(touchEvent);
+    }
+
+    document.addEventListener('mousedown', function(event) {
+        emitTouchEvent(event, 'touchstart');
+    });
+
+    document.addEventListener('mousemove', function(event) {
+        emitTouchEvent(event, 'touchmove');
+    });
+
+    document.addEventListener('mouseup', function(event) {
+        emitTouchEvent(event, 'touchend');
+    });
+
+})();
